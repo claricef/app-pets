368148e6f66b5016ee01bb69dc84d304d75504cb
claricef <clariceoliver432@gmail.com>
2022-01-25 20:00:09 -0300

    adicionando a rota home

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Switch, Route, BrowserRouter} from 'react-router-dom';
 import New from './pages/Pet/new';
 import Show from './pages/Pet/show';
+import Home from './pages/Home';
 
 const Routes  =  () =>{
     return(
         <BrowserRouter>
             <Switch>
+                <Route exact path="/" component={Home} />
                 <Route exact path="/new" component={New} />
                 <Route exact path="/show" component={Show}/>
             </Switch>
